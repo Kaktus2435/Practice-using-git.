cc6e7a8f5d0ce9a654e98f221f636addc861770e
Paereli Andrian <aliansperfect@gmail.com>
2025-03-19 12:30:44 +0200

    [PA 5 change]

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ import Text from './components/text';
 function App() {
   return (
     <div className="App">
-      <h1 style={{color: "red"}} >4 change</h1>
+      <h1 style={{color: "red"}} >6 change</h1>
 
       <Text />
     </div>
