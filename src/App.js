9b15b78f51f45b975267025e514f99c4fbbafdac
Paereli Andrian <aliansperfect@gmail.com>
2025-03-19 10:29:00 +0200

    4 change

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <h1 style={{color: "red"}} >Second change</h1>
+      <h1 style={{color: "red"}} >3 change</h1>
     </div>
   );
 }
